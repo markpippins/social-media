6f23865af92089280924e1e7408cb0c084903c4a
Mark Pippins <mpippins@gmail.com>
2021-05-16 11:59:24 -0400

    stubbed out client-side jwt

--- webapp/src/app/services/auth/auth.service.ts
+++ webapp/src/app/services/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { UserService } from '../user.service';
+import { JwtService } from './jwt.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private jwtService: JwtService) { }
 
   public isAuthenticated(): boolean {
     const token = localStorage.getItem('token');
