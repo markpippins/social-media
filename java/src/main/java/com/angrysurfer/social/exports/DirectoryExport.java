7249e502d19adf8fd1a47d06862cf68ea04473f9
Mark Pippins <mpippins@gmail.com>
2021-08-03 14:28:29 -0400

    added swagger (again)

--- java/src/main/java/com/angrysurfer/social/exports/DirectoryExport.java
+++ java/src/main/java/com/angrysurfer/social/exports/DirectoryExport.java
@@ -5,12 +5,10 @@ import com.angrysurfer.social.shrapnel.TabularExport;
 import com.angrysurfer.social.shrapnel.component.ColumnSpec;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class DirectoryExport extends TabularExport {
 
@@ -27,7 +25,13 @@ public class DirectoryExport extends TabularExport {
 
         @Override
         public Collection<Object> getData() {
-            return Stream.of(new File(".").listFiles()).collect(Collectors.toList());
+
+//            File file = new File(".");
+//            String fontProperties = file.getAbsolutePath().substring(0, file.getAbsolutePath().length() - 1) + FONTS_FOLDER;
+//
+//            List<Object> data = Stream.of(new File(fontProperties).listFiles()).collect(Collectors.toList());
+//            return data;
+            return Collections.EMPTY_LIST;
         }
 
         @Override
